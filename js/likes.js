be3a77b398b7aa1c915c8d0ee148e59aeb6372e2
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-07-15 10:47:14 +0200

    Mise à jour du total de likes lorque l'on clique sur le like d'une image

---
+++ js/likes.js
@@ -0,0 +1,40 @@
+function manageLikes() {
+  let totalLikesDisplay = document.getElementById("total-likes");
+  let likeButtons = document.querySelectorAll(".like-button");
+  let arrayOfLikes = [];
+
+  likeButtons.forEach((likeButton, i) => {
+    likeButton.addEventListener("click", function () {
+      let isLiked = likeButtons[i].classList.toggle("is-liked");
+      let initialValue = document.querySelectorAll(".image-likes");
+      if (isLiked == true) {
+        let newValue = parseInt(initialValue[i].innerText) + 1;
+        initialValue[i].innerText = newValue;
+      } else {
+        newValue = parseInt(initialValue[i].innerText) - 1;
+        initialValue[i].innerText = newValue;
+      }
+    });
+    let totalLikes = parseInt(
+      document.querySelectorAll(".image-likes")[i].innerText
+    );
+    arrayOfLikes.push(totalLikes);
+    console.log(arrayOfLikes);
+  });
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  let sumTotalLikes = arrayOfLikes.reduce(reducer);
+  totalLikesDisplay.innerHTML = sumTotalLikes;
+
+  // Mise à jour de la somme totale des likes en prenant en compte les nouveaux "likes" likés
+  likeButtons.forEach((likeButton, i) => {
+    likeButton.addEventListener("click", function () {
+      let likedImage = document.getElementsByClassName("is-liked");
+      console.log(likedImage);
+      let addNewLike = likedImage.length;
+      console.log(addNewLike);
+      let newTotalLikes = sumTotalLikes + addNewLike;
+      totalLikesDisplay.innerText = newTotalLikes;
+    });
+  });
+}
--- js/photographer.js
+++ js/photographer.js
@@ -16,8 +16,8 @@ fetch("../data/FishEyeData.json")
   .then((data) => {
     let photographers = data.photographers;
     let medias = data.media;
-    console.log(photographers);
-    console.log(medias);
+    //console.log(photographers);
+    //console.log(medias);
     for (let i in photographers) {
       let photographerId = photographers[i].id;
       if (photographerId == id) {
@@ -26,7 +26,6 @@ fetch("../data/FishEyeData.json")
         addPhotographerPrice(photographers[i]);
       }
     }
-
     for (let i in medias) {
       let mediaId = medias[i].photographerId;
       //console.log(mediaId);
@@ -35,11 +34,6 @@ fetch("../data/FishEyeData.json")
         //sortByDate(medias[i]);
       }
     }
-
-    manageLikes();
-    //addLike();
-    //addPhotographerTotalLikes();
-
     let contactBtn = document.querySelector(".contact-btn");
     console.log(contactBtn);
     contactBtn.addEventListener("click", function () {
@@ -50,19 +44,11 @@ fetch("../data/FishEyeData.json")
     closeBtn.addEventListener("click", function () {
       modalForm.style.display = "none";
     });
-  });
-
-/*function sortByDate(media) {
-  let date =[ media.date];
-  console.log(date)
-  date.sort(function(a, b) {
-    return a - b;
    
 
+    manageLikes();
+    
   });
-  console.log(date); 
-}
-*/
 
 function addNameInForm(photographer) {
   let photographerName = document.getElementById("test");
@@ -107,72 +93,4 @@ function addPhotographerPrice(photographer) {
   console.log(prices);
 }
 
-/*function addLike() {
-  let likeButton = document.querySelectorAll(".like-button");
-  let imageLikes = document.querySelectorAll(".image-likes");
-  console.log(likeButton);
-  console.log(imageLikes);
-  for (let i in likeButton) {
-    likeButton[i].addEventListener("click", function () {
-      let imageNumber = parseInt(imageLikes[i].innerText) + 1;
-      imageLikes[i].innerText = imageNumber;
-      console.log(imageNumber);
-    });
-  }
-}
 
-function addPhotographerTotalLikes() {
-  let totalLikes = document.getElementById("total-likes");
-  let arrayOfLikes = [];
-  let imageLikes = document.querySelectorAll(".image-likes");
-  console.log(imageLikes.length);
-  for (let i in imageLikes) {
-    let imageNumber = parseInt(imageLikes[i].innerText);
-    if (isNaN(imageNumber)) {
-      return 0;
-    } else {
-      arrayOfLikes.push(imageNumber);
-      console.log(arrayOfLikes);
-      const reducer = (accumulator, currentValue) => accumulator + currentValue;
-      let sumTotalLikes = arrayOfLikes.reduce(reducer);
-      console.log(sumTotalLikes);
-      totalLikes.innerHTML = sumTotalLikes;
-    }
-  }
-}
-*/
-
-function manageLikes() {
-  let totalLikes = document.getElementById("total-likes");
-  let likeButton = document.querySelectorAll(".like-button");
-  let imageLikes = document.querySelectorAll(".image-likes");
-  let arrayOfLikes = [];
-  for (let i in likeButton) {
-    likeButton[i].addEventListener("click", function () {
-      let isLiked = likeButton[i].classList.toggle("is-liked");
-      if ((likeButton[i] = isLiked)) {
-        let imageNumber = parseInt(imageLikes[i].innerText) + 1;
-        imageLikes[i].innerText = imageNumber;
-        console.log(imageNumber);
-      } else {
-        let imageNumber = parseInt(imageLikes[i].innerText) - 1;
-        imageLikes[i].innerText = imageNumber;
-        console.log(imageNumber);
-      }
-    });
-    for (let i in imageLikes) {
-      let imageNumber = parseInt(imageLikes[i].innerText);
-      if (isNaN(imageNumber)) {
-        return 0;
-      } else {
-        arrayOfLikes.push(imageNumber);
-        console.log(arrayOfLikes);
-        const reducer = (accumulator, currentValue) =>
-          accumulator + currentValue;
-        let sumTotalLikes = arrayOfLikes.reduce(reducer);
-        console.log(sumTotalLikes);
-        totalLikes.innerHTML = sumTotalLikes;
-      }
-    }
-  }
-}

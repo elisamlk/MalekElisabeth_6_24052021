37817b86ac00138471910da6d67e407209fa2dda
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-08-17 07:47:46 +0200

    Evenement pour faire défiler les différents medias

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,25 +4,35 @@ function displayLightbox() {
   let lightboxNext = document.querySelector(".fa-chevron-right");
   let lightboxPrev = document.querySelector(".fa-chevron-left");
   let lightboxMedias = document.querySelectorAll(".links");
-  // On écoute l'évenement sur les medias
-  for (let lightboxMedia of lightboxMedias) {
-    //console.log(lightboxMedia)
-    lightboxMedia.addEventListener("click", function () {
-      console.log(lightboxMedia);
-      
-      //let mediaType = document.querySelector(".media-type");
+  let lightboxArray = Array.from(lightboxMedias);
+  console.log(lightboxArray);
+  //let lastImage = lightboxArray.length -1;
 
+  for (let lightboxMedia of lightboxMedias) {
+    lightboxMedia.addEventListener("click", function () {
+      let typeOfMedia = lightboxMedia.childNodes[1];
+      console.log(lightbox);
       // On ajoute le média cliqué dans la modale
+      let mediaType = document.querySelector(".media-type");
+      mediaType.appendChild(typeOfMedia);
 
       // On affiche la lightbox
       lightbox.classList.add("show");
+
+      //On fait défiler les photos
+      lightboxNext.addEventListener("click", function () {
+        let activeMedia = lightboxArray.indexOf(lightboxMedia);
+        console.log(activeMedia);
+        mediaType.append(lightboxArray[activeMedia +1]);
+        mediaType.removeChild(typeOfMedia);
+      });
+
       // On retire la classe qui permet d'afficher la lightbox
       lightboxClose.addEventListener("click", function () {
         lightbox.classList.remove("show");
+        mediaType.removeChild(typeOfMedia);
+        lightboxMedia.append(typeOfMedia);
       });
     });
   }
 }
-
-
-

53fc22ac5f0ac2da1954c803cc6d145dffc008b1
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-08-23 14:47:57 +0200

    Faire défiler les medias vers la gauche

--- js/lightbox.js
+++ js/lightbox.js
@@ -5,33 +5,53 @@ function displayLightbox() {
   let lightboxPrev = document.querySelector(".fa-chevron-left");
   let lightboxMedias = document.querySelectorAll(".links");
   let lightboxArray = Array.from(lightboxMedias);
-  console.log(lightboxArray);
-  //let lastImage = lightboxArray.length -1;
+  console.log(lightboxArray)
+  let lastImage = lightboxArray.length - 1;
 
   for (let lightboxMedia of lightboxMedias) {
     lightboxMedia.addEventListener("click", function () {
       let typeOfMedia = lightboxMedia.childNodes[1];
-      console.log(lightbox);
+     
       // On ajoute le média cliqué dans la modale
       let mediaType = document.querySelector(".media-type");
       mediaType.appendChild(typeOfMedia);
+      let activeMedia = lightboxArray.indexOf(lightboxMedia);
+      console.log(activeMedia);
+      mediaType.setAttribute("currentIndex", activeMedia);
+      console.log(mediaType);
 
       // On affiche la lightbox
       lightbox.classList.add("show");
 
-      //On fait défiler les photos
+      //On fait défiler les photos vers la droite
       lightboxNext.addEventListener("click", function () {
-        let activeMedia = lightboxArray.indexOf(lightboxMedia);
-        console.log(activeMedia);
-        mediaType.append(lightboxArray[activeMedia +1]);
-        mediaType.removeChild(typeOfMedia);
+        mediaType.innerHTML = "";
+        let currentIndex = parseInt(mediaType.getAttribute("currentIndex"));
+        mediaType.append(lightboxArray[currentIndex + 1]);
+        mediaType.setAttribute("currentIndex", currentIndex + 1);
+      });
+
+      //On fait défiler les photos vers la gauche
+      lightboxPrev.addEventListener("click", function () {
+        mediaType.innerHTML = "";
+        let currentIndex = parseInt(mediaType.getAttribute("currentIndex"));
+        console.log(currentIndex - 1);
+        mediaType.append(lightboxArray[currentIndex - 1]);
+        mediaType.setAttribute("currentIndex", currentIndex - 1);
       });
 
       // On retire la classe qui permet d'afficher la lightbox
       lightboxClose.addEventListener("click", function () {
+        //lightbox.classList.remove("show");
+        //lightboxMedia.append(typeOfMedia);
+        //mediaType.removeChild(typeOfMedia);
         lightbox.classList.remove("show");
-        mediaType.removeChild(typeOfMedia);
-        lightboxMedia.append(typeOfMedia);
+        
+        
+        console.log(portfolio);
+
+
+
       });
     });
   }

18c9cad331968ce6cac8d760553141143fcf8ab4
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-09-02 15:41:42 +0200

    Finalisation du javascript de la lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,24 +1,25 @@
 function displayLightbox() {
   let lightbox = document.querySelector(".lightbox");
+  let lightboxMedias = document.querySelectorAll(".links");
   let lightboxClose = document.querySelector(".lightbox-close");
   let lightboxNext = document.querySelector(".fa-chevron-right");
   let lightboxPrev = document.querySelector(".fa-chevron-left");
-  let lightboxMedias = document.querySelectorAll(".links");
+
   let lightboxArray = Array.from(lightboxMedias);
-  console.log(lightboxArray)
-  let lastImage = lightboxArray.length - 1;
+  console.log(lightboxArray);
+  let lastImageIndex = lightboxArray.length - 1;
+  console.log(lastImageIndex);
 
   for (let lightboxMedia of lightboxMedias) {
     lightboxMedia.addEventListener("click", function () {
       let typeOfMedia = lightboxMedia.childNodes[1];
-     
+
       // On ajoute le média cliqué dans la modale
       let mediaType = document.querySelector(".media-type");
-      mediaType.appendChild(typeOfMedia);
+      let cloneUn = typeOfMedia.cloneNode(true);
+      mediaType.appendChild(cloneUn);
       let activeMedia = lightboxArray.indexOf(lightboxMedia);
-      console.log(activeMedia);
       mediaType.setAttribute("currentIndex", activeMedia);
-      console.log(mediaType);
 
       // On affiche la lightbox
       lightbox.classList.add("show");
@@ -27,32 +28,40 @@ function displayLightbox() {
       lightboxNext.addEventListener("click", function () {
         mediaType.innerHTML = "";
         let currentIndex = parseInt(mediaType.getAttribute("currentIndex"));
-        mediaType.append(lightboxArray[currentIndex + 1]);
-        mediaType.setAttribute("currentIndex", currentIndex + 1);
+        console.log(currentIndex);
+        if (currentIndex != lastImageIndex) {
+          let clone = lightboxArray[currentIndex + 1].cloneNode(true);
+          mediaType.append(clone);
+          mediaType.setAttribute("currentIndex", currentIndex + 1);
+        } else {
+          mediaType.innerHTML = "";
+          mediaType.append(lightboxArray[0].cloneNode(true));
+          mediaType.setAttribute("currentIndex", currentIndex - lastImageIndex);
+        }
       });
 
       //On fait défiler les photos vers la gauche
       lightboxPrev.addEventListener("click", function () {
         mediaType.innerHTML = "";
         let currentIndex = parseInt(mediaType.getAttribute("currentIndex"));
-        console.log(currentIndex - 1);
-        mediaType.append(lightboxArray[currentIndex - 1]);
-        mediaType.setAttribute("currentIndex", currentIndex - 1);
+        if (currentIndex != 0) {
+          let clone = lightboxArray[currentIndex - 1].cloneNode(true);
+          mediaType.append(clone);
+          mediaType.setAttribute("currentIndex", currentIndex - 1);
+        } else if (currentIndex == 0) {
+          mediaType.innerHTML = "";
+          mediaType.append(lightboxArray[lastImageIndex].cloneNode(true));
+          mediaType.setAttribute("currentIndex", currentIndex + lastImageIndex);
+        }
       });
 
       // On retire la classe qui permet d'afficher la lightbox
       lightboxClose.addEventListener("click", function () {
-        //lightbox.classList.remove("show");
-        //lightboxMedia.append(typeOfMedia);
-        //mediaType.removeChild(typeOfMedia);
         lightbox.classList.remove("show");
-        
-        
-        console.log(portfolio);
-
-
-
+        mediaType.innerHTML = "";
       });
     });
   }
 }
+
+

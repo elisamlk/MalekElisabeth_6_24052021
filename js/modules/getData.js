1d95e3ef88227fce60a502e507bd0b940dfa87d9
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-09-16 14:59:23 +0200

    Modif getData

--- js/modules/getData.js
+++ js/modules/getData.js
@@ -1,2 +1,7 @@
 export const getData = () =>
-  fetch("./data/FishEyeData.json").then((response) => response.json());
+fetch("/data/FishEyeData.json")
+    .then((response) => response.json())
+    .catch((error) => {
+        console.error("Une erreur est survenue pendant l'accès aux données.");
+        console.error(error);
+    });

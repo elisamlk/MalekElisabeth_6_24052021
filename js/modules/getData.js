f37b49b1a94a3ee4b060b18b172f04715a7ee3a5
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-09-17 23:32:38 +0200

    fix githubPages

--- js/modules/getData.js
+++ js/modules/getData.js
@@ -1,5 +1,5 @@
 export const getData = () =>
-fetch("./data/FishEyeData.json")
+fetch("../data/FishEyeData.json")
     .then((response) => response.json())
     .catch((error) => {
         console.error("Une erreur est survenue pendant l'accès aux données.");

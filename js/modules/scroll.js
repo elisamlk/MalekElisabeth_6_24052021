96a2a97a87cbd26612ee275ede34a84a24f9e3cf
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2021-09-27 14:48:19 +0200

    fonction retour en haut de page

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -8,4 +8,8 @@ export function scrollButton() {
       scroll.style.display = "none";
     }
   });
+
+  scroll.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+  });
 }
